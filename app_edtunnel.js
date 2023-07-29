5002891f59b3200dcb2b250027e645dacdb435c5
sailor89 <58934121+sailor89@users.noreply.github.com>
2023-07-29 17:39:28 +0800

    Update app_edtunnel.js

--- app_edtunnel.js
+++ app_edtunnel.js
@@ -4,7 +4,7 @@ const net = require('net');
 
 // 创建 WebSocket 服务器
 const wss = new WebSocket.Server({ port: 443 });
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '71234bfd-7d07-45a4-9765-97b741e70fe8';
 let proxyIP = "64.68.192." + Math.floor(Math.random() * 255);
 
 let address = '';
